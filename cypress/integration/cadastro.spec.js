4589c6c087b52f49d019c664c0e6e4ddc7ba9069
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2022-04-19 22:16:39 -0300

    :sparkles: #1 Combinando CSS Seletor com texto

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -39,5 +39,9 @@ describe('Cadastro', () => {
         cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
         cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
         cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
+
+        //cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+        cy.get('img[alt="Moto"]').click()
+
     })
 })
