35933b3456a71f16040164af068953e4a00bc202
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2022-04-20 20:26:59 -0300

    :recycle: #1 Refatorando a massa de testes para inglês

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -8,37 +8,37 @@ describe('Cadastro', () => {
         cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
 
         //Criar massa de teste
-        var entregador ={
-            nome: 'Janaina Feitosa',
+        var deliver ={
+            name: 'Janaina Feitosa',
             cpf: '00000014141',
             email: 'janaina@email.com',
             whatsapp: '11999999999',
-            endereco: {
-                cep: '63031280',
-                rua: 'Rua Coronel José Moreira Cabral',
-                numero: '306',
-                complemento: 'Casa',
-                bairro: 'Tiradentes',
-                cidade_uf: 'Juazeiro do Norte/CE'
+            address: {
+                postalcode: '63031280',
+                street: 'Rua Coronel José Moreira Cabral',
+                number: '306',
+                details: 'Casa',
+                district: 'Tiradentes',
+                city_state: 'Juazeiro do Norte/CE'
             },
-            metodo_entrega: 'Moto',
+            delivery_method: 'Moto',
             cnh: 'cnh-digital.jpg'
         }
 
-        cy.get('input[name="name"]').type(entregador.nome)
-        cy.get('input[name="cpf"]').type(entregador.cpf) 
-        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.cpf) 
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
 
-        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
         cy.get('input[type=button][value="Buscar CEP"]').click()
 
-        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
-        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+        cy.get('input[name="address-number"]').type(deliver.address.number)
+        cy.get('input[name="address-details"]').type(deliver.address.details)
 
         //Verificação das informações inseridas
-        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
-        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
-        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
+        cy.get('input[name="address"]').should('have.value', deliver.address.street)
+        cy.get('input[name="district"]').should('have.value', deliver.address.district)
+        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
 
         //cy.contains('.delivery-method li', entregador.metodo_entrega).click()
         cy.get('img[alt="Moto"]').click()
@@ -46,7 +46,7 @@ describe('Cadastro', () => {
         /*Expressões regulares
           Acento ^ é para dizer que a propriedade começa com algo
           $ é para dizer que a propriedade termina com */
-        cy.get('input[accept^="image"]').attachFile('/images/' + entregador.cnh)
+        cy.get('input[accept^="image"]').attachFile('/images/' + deliver.cnh)
 
         cy.get('form button[type="submit"]').click()
 
@@ -56,53 +56,46 @@ describe('Cadastro', () => {
     })
 
     it('CPF incorreto', () => {
-        //Define dimensões de tela
         cy.viewport(1440, 900)
         cy.visit('https://buger-eats.vercel.app')
 
         cy.get('a[href="/deliver"]').click()
         cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
 
-        //Criar massa de teste
-        var entregador ={
-            nome: 'Janaina Feitosa',
+        var deliver ={
+            name: 'Janaina Feitosa',
             cpf: '000000141AA',
             email: 'janaina@email.com',
             whatsapp: '11999999999',
-            endereco: {
-                cep: '63031280',
-                rua: 'Rua Coronel José Moreira Cabral',
-                numero: '306',
-                complemento: 'Casa',
-                bairro: 'Tiradentes',
-                cidade_uf: 'Juazeiro do Norte/CE'
+            address: {
+                postalcode: '63031280',
+                street: 'Rua Coronel José Moreira Cabral',
+                number: '306',
+                details: 'Casa',
+                district: 'Tiradentes',
+                city_state: 'Juazeiro do Norte/CE'
             },
-            metodo_entrega: 'Moto',
+            delivery_method: 'Moto',
             cnh: 'cnh-digital.jpg'
         }
 
-        cy.get('input[name="name"]').type(entregador.nome)
-        cy.get('input[name="cpf"]').type(entregador.cpf) 
-        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.cpf) 
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
 
-        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
         cy.get('input[type=button][value="Buscar CEP"]').click()
 
-        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
-        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+        cy.get('input[name="address-number"]').type(deliver.address.number)
+        cy.get('input[name="address-details"]').type(deliver.address.details)
 
-        //Verificação das informações inseridas
-        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
-        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
-        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
-
-        //cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+        cy.get('input[name="address"]').should('have.value', deliver.address.street)
+        cy.get('input[name="district"]').should('have.value', deliver.address.district)
+        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
+        
         cy.get('img[alt="Moto"]').click()
 
-        /*Expressões regulares
-          Acento ^ é para dizer que a propriedade começa com algo
-          $ é para dizer que a propriedade termina com */
-        cy.get('input[accept^="image"]').attachFile('/images/' + entregador.cnh)
+        cy.get('input[accept^="image"]').attachFile('/images/' + deliver.cnh)
 
         cy.get('form button[type="submit"]').click()
 
